07c58860bdb51832e11ca34fa6a7a4b8eaa892f9
khonda <82398768+Khondamir26@users.noreply.github.com>
2026-02-28 03:24:25 +0500

    ci/cd

--- Birga_Quramiz_Frontend/next.config.ts
+++ Birga_Quramiz_Frontend/next.config.ts
@@ -4,11 +4,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'}/:path*`,
       },
     ]
   },
