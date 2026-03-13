773b0a2febd3fc285e3fdc0546e9f70febd36aa8
khonda <82398768+Khondamir26@users.noreply.github.com>
2026-03-14 04:43:37 +0500

    fix eslint

--- Birga_Quramiz_Backend/src/app.service.ts
+++ Birga_Quramiz_Backend/src/app.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, ServiceUnavailableException } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
 import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
@@ -12,7 +11,7 @@ export class AppService {
 
   async getHealth() {
     try {
-      await this.prisma.$queryRaw(Prisma.sql`SELECT 1`);
+      await this.prisma.$queryRaw`SELECT 1`;
       return {
         status: 'ok',
         timestamp: new Date().toISOString(),
