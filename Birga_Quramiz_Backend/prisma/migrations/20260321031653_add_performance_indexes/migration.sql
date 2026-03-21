6dc89f01a51186c4ac0a14dc45c75931c9988605
khonda <82398768+Khondamir26@users.noreply.github.com>
2026-03-21 08:40:14 +0500

    fix: migration

--- Birga_Quramiz_Backend/prisma/migrations/20260321031653_add_performance_indexes/migration.sql
+++ Birga_Quramiz_Backend/prisma/migrations/20260321031653_add_performance_indexes/migration.sql
@@ -1,8 +1,8 @@
 -- DropIndex
-DROP INDEX "Order_createdAt_idx";
+DROP INDEX IF EXISTS "Order_createdAt_idx";
 
 -- DropIndex
-DROP INDEX "Product_status_createdAt_idx";
+DROP INDEX IF EXISTS "Product_status_createdAt_idx";
 
 -- CreateIndex
 CREATE INDEX "Order_createdAt_idx" ON "Order"("createdAt");
